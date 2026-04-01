1a2fd84fe805dbf121b371afb5277a719f4228d7
Dmitriy <schajckodmitrij@gmail.com>
2026-04-01 19:19:54 +0300

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RewardsModule } from './rewards/rewards.module';
-import { MerkleService } from './merkle/merkle.service';
-import { MerkleModule } from './merkle/merkle.module';
 import { NodesModule } from './nodes/nodes.module';
-import { SubscriptionsModule } from './subscriptions/subscriptions.module';
 import { TonService } from './ton/ton.service';
 import { ConfigModule } from '@nestjs/config';
 import configuration from './configuration';
@@ -22,11 +19,9 @@ import { TaskController } from './task/task.controller';
             validationSchema,
         }),
         RewardsModule,
-        MerkleModule,
         NodesModule,
-        SubscriptionsModule,
     ],
     controllers: [AppController, TaskController],
-    providers: [AppService, MerkleService, TonService, TaskService],
+    providers: [AppService, TonService, TaskService],
 })
 export class AppModule {}
