1321eb8730390f686b129054fc8e60541d440dc1
Dmitriy <schajckodmitrij@gmail.com>
2026-04-01 21:18:54 +0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 4000);
+    const app = await NestFactory.create(AppModule);
+
+    app.enableCors({
+        origin: ['http://localhost:3000', 'https://app.apivpn.dev/rewards'],
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        credentials: true,
+    });
+
+    await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
