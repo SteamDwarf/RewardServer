0b57c1ebfa2b152c0a0e393ca2bec925e01bc492
Dmitriy <schajckodmitrij@gmail.com>
2026-04-02 12:00:16 +0300

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
     app.enableCors({
-        origin: ['http://localhost:5173', 'https://app.apivpn.dev/rewards'],
+        origin: ['http://localhost:5173', 'https://app.apivpn.dev'],
         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
         credentials: true,
     });
